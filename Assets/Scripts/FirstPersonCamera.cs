51e53fd40d28e9e45a9da2afb3355cd812a3f090
David Burke <burkedm94@gmail.com>
2024-11-19 18:27:10 -0500

    highlight poly selected

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -15,16 +15,14 @@ public class FirstPersonCamera : MonoBehaviour
     private float xRotation = 0f;
     private MeshFilter lastHighlightedMeshFilter;
     private int[] lastHighlightedTriangleIndices;
-    private Color originalColor = Color.white;
     private Color highlightColor = Color.red;
-    public Material highlightMaterial;
-    private float highlightIntensity = 0f;
+
+    private Dictionary<MeshFilter, Color[]> originalColorsCache = new Dictionary<MeshFilter, Color[]>();
 
     private void Start()
     {
         cameraComponent = GetComponent<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
-        highlightMaterial.SetFloat("_highlightIntensity", highlightIntensity);
     }
 
     private void Update()
@@ -37,6 +35,11 @@ public class FirstPersonCamera : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
 
+        HandleMeshHighlighting();
+    }
+
+    private void HandleMeshHighlighting()
+    {
         Ray ray = cameraComponent.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
@@ -44,76 +47,87 @@ public class FirstPersonCamera : MonoBehaviour
             if (meshFilter != null)
             {
                 Mesh mesh = meshFilter.mesh;
-                Vector3 planetCenter = planet.gameObject.transform.localPosition;
-                Vector3[] vertices = mesh.vertices;
-                Color[] colors = mesh.colors.Length == vertices.Length ? mesh.colors : new Color[vertices.Length];
-                if (colors.Length != vertices.Length)
-                {
-                    for (int i = 0; i < vertices.Length; i++)
-                    {
-                        colors[i] = originalColor;
-                    }
-                }    
 
+                if (!originalColorsCache.ContainsKey(meshFilter))
+                {
+                    originalColorsCache[meshFilter] = mesh.colors.Length == mesh.vertexCount ? (Color[])mesh.colors.Clone() : Enumerable.Repeat(Color.white, mesh.vertexCount).ToArray();
+                }
 
                 if (lastHighlightedMeshFilter != null && lastHighlightedTriangleIndices != null)
                 {
-                    for (int i = 0; i < lastHighlightedTriangleIndices.Length; i++)
-                    {
-                        colors[lastHighlightedTriangleIndices[i]] = originalColor;
-                    }
-                    lastHighlightedMeshFilter.mesh.colors = colors;
+                    RestoreOriginalColors(lastHighlightedMeshFilter);
                 }
 
-                int[] triangles = mesh.triangles;
-                int triangleIndex = hit.triangleIndex;
+                HighlightTriangle(meshFilter, hit);
 
-                int vert1 = triangles[triangleIndex * 3];
-                int vert2 = triangles[triangleIndex * 3 + 1];
-                int vert3 = triangles[triangleIndex * 3 + 2];
-                lastHighlightedTriangleIndices = new int[] {vert1, vert2, vert3};
-
-                foreach (int index in lastHighlightedTriangleIndices)
-                {
-                    colors[index] = highlightColor;
-                }
-
-                highlightIntensity = 1f;
-                highlightMaterial.SetFloat("_highlightIntensity", highlightIntensity);
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    float vertHeight1 = (vertices[vert1] - planetCenter).magnitude;
-                    float vertHeight2 = (vertices[vert2] - planetCenter).magnitude;
-                    float vertHeight3 = (vertices[vert3] - planetCenter).magnitude;
-                    List<(int index, float height)> verts = new List<(int, float)> { 
-                        (vert1, vertHeight1), 
-                        (vert2, vertHeight2), 
-                        (vert3, vertHeight3) 
-                    };
-
-                    int highestVert = verts.OrderByDescending(x => x.height).First().index;
-
-                    vertices[highestVert] = MoveVertexTowardCenter(vertices[highestVert], planetCenter, 1f);
-
-                }
-
-                mesh.colors = colors;
                 lastHighlightedMeshFilter = meshFilter;
-                mesh.vertices = vertices;
-                mesh.RecalculateNormals();
-                mesh.RecalculateBounds();
-                meshFilter.mesh = mesh;
             }
         }
-        else
+        else if (lastHighlightedMeshFilter != null)
         {
-            highlightIntensity = 1f;
-            highlightMaterial.SetFloat("_highlightIntensity", highlightIntensity);
+            RestoreOriginalColors(lastHighlightedMeshFilter);
+            lastHighlightedMeshFilter = null;
         }
     }
 
-    private Vector3 MoveVertexTowardCenter(Vector3 vertex, Vector3 center, float amount) {
+    private void HighlightTriangle(MeshFilter meshFilter, RaycastHit hit)
+    {
+        Mesh mesh = meshFilter.mesh;
+        Color[] colors = mesh.colors.Length == mesh.vertexCount ? mesh.colors : Enumerable.Repeat(Color.white, mesh.vertexCount).ToArray();
+
+        int[] triangles = mesh.triangles;
+        int triangleIndex = hit.triangleIndex;
+
+        int vert1 = triangles[triangleIndex * 3];
+        int vert2 = triangles[triangleIndex * 3 + 1];
+        int vert3 = triangles[triangleIndex * 3 + 2];
+        lastHighlightedTriangleIndices = new int[] { vert1, vert2, vert3 };
+
+        foreach (int index in lastHighlightedTriangleIndices)
+        {
+            colors[index] = highlightColor;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3[] vertices = mesh.vertices;
+            Vector3 planetCenter = planet.gameObject.transform.localPosition;
+            float vertHeight1 = (vertices[vert1] - planetCenter).magnitude;
+            float vertHeight2 = (vertices[vert2] - planetCenter).magnitude;
+            float vertHeight3 = (vertices[vert3] - planetCenter).magnitude;
+            List<(int index, float height)> verts = new List<(int, float)> {
+                            (vert1, vertHeight1),
+                            (vert2, vertHeight2),
+                            (vert3, vertHeight3)
+                        };
+
+            int highestVert = verts.OrderByDescending(x => x.height).First().index;
+
+            vertices[highestVert] = MoveVertexTowardCenter(vertices[highestVert], planetCenter, 1f);
+
+            mesh.vertices = vertices;
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+            meshFilter.mesh = mesh;
+
+        }
+
+        mesh.colors = colors;
+        Debug.Log($"Highlighting vertices: {string.Join(", ", lastHighlightedTriangleIndices)}");
+    }
+
+    private void RestoreOriginalColors(MeshFilter meshFilter)
+    {
+        if (originalColorsCache.TryGetValue(meshFilter, out Color[] originalColors))
+        {
+            meshFilter.mesh.colors = (Color[])originalColors.Clone();
+        }
+    }
+
+
+
+    private Vector3 MoveVertexTowardCenter(Vector3 vertex, Vector3 center, float amount)
+    {
         float minRadius = (float)Math.Floor((planet.shapeSettings.planetRadius));
         Vector3 directionToCenter = (center - vertex).normalized;
         Vector3 newVertex = vertex + directionToCenter * amount;
@@ -126,3 +140,5 @@ public class FirstPersonCamera : MonoBehaviour
         return vertex;
     }
 }
+
+
