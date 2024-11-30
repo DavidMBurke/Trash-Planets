6511c282d1bb872cf444513e7dc752f602e88c9a
Ayden Johnson <67842316+ADJohnson314@users.noreply.github.com>
2024-11-29 23:11:43 -0600

    Add visual inventory status display
    Includes display of quantity of trash and building mats, as well as description of building being placed. Still needs work.

---
+++ Assets/Scripts/UpdateBuildingDisplay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateBuildingDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI BuildingText;
+    public GameObject Camera;
+    private FirstPersonCamera cameraScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildingText = GetComponent<TextMeshProUGUI>();
+        if (BuildingText == null)
+        {
+            Debug.LogError("BuildingDisplay object is missing a TextMeshProUGUI component.");
+            return;
+        }
+
+        cameraScript = Camera.GetComponent<FirstPersonCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject building = cameraScript.selectedPrefab;
+
+        if (building != null) {
+            BuildingText.text = $"Placing building: {building.tag}";
+        }
+        else {
+            BuildingText.text = $"Mining";
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateInventoryCounts.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateInventoryCounts : MonoBehaviour
+{
+    private TextMeshProUGUI InventoryText;
+    public GameObject PlayerObject;
+    private Player playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InventoryText = GetComponent<TextMeshProUGUI>();
+        if (InventoryText == null)
+        {
+            Debug.LogError("InventoryCounts object is missing a TextMeshProUGUI component.");
+            return;
+        }
+
+        playerScript = PlayerObject.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int trash = playerScript.trashQty;
+        int building_mats = playerScript.building_mat_qty;
+
+        InventoryText.text = $"Trash: {trash}\nBuilding Materials: {building_mats}";
+        
+    }
+}
