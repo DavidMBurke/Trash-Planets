a5d26562aa2b5a8460db87fbc130ed517e592fd0
TheMomaek <junji@mit.edu>
2024-12-09 23:57:36 -0500

    Better Menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.EventSystems;
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource buttonclicksound;
+
+    public GameObject RulesetFirstSelect, NormalFirstSelect, CloseFirstSelect;
     public void StartGame()
     {
+        if (!buttonclicksound.isPlaying)
+            buttonclicksound.Play();
         SceneManager.LoadSceneAsync("Production");
     }
 
+    public void RulesetOpen(){
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(RulesetFirstSelect);
+    }
+    public void RulesetClose(){
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(CloseFirstSelect);
+    }
     public void QuitGame()
     {
+        if (!buttonclicksound.isPlaying)
+            buttonclicksound.Play();
         Application.Quit();
     }
 }
